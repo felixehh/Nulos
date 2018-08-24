4144c785716f1e8b433b8b6354991c4d60e30a31
felixehh <42181298+felixehh@users.noreply.github.com>
2018-08-24 07:48:25 -0700

    Initial commit

---
+++ Dueño.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nulos
+{
+    class Dueño
+    {
+        public string Nombre { get; set; }
+        public string FechaNacimiento { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro miCarro = new Carro();
+            miCarro.Marca = "Tsuru";
+            miCarro.Kilometraje = 100000;
+
+            Console.WriteLine("Marca: " + miCarro.Marca);
+            Console.WriteLine("Km: " + miCarro.Kilometraje);
+
+            if(miCarro.DueñoCarro != null)
+            {
+                Console.WriteLine("Dueño: " + miCarro.DueñoCarro.Nombre);
+            }
+            else
+            {
+                Console.WriteLine("No se encontro dueño");
+            }
+
+            //Console.WriteLine("Mi carro tiene: " + miCarro.Kilometraje + " km recorridos");
+
+            if (miCarro == null)
+            {
+                Console.WriteLine("no ha sido asignado un valor");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
